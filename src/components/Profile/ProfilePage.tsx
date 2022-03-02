b125bb1471f3bcc3be11baf236a848031cb4d7a0
Andrey <brodiaga_0880@mail.ru>
2022-03-03 00:09:30 +0300

    v 0.9.7.1

--- src/components/Profile/ProfilePage.tsx
+++ src/components/Profile/ProfilePage.tsx
@@ -103,10 +103,54 @@ export const ProfilePage: FC = () => {
   return (
     <>
       <div className="profile bannerBack">
-        <div className="profilePreview">
-          <div className="profilePreview__Title">
-            <b>Ваш персонаж</b>
+        <div className="profileMenu">
+          <div className="profileMenuTitle">Добро пожаловать</div>
+          <div className="profileMenuList">
+            <div className="profileMenuList__items">
+              {filteredPersons.length === 0
+                ? 'загрузка'
+                : <>
+                  {filteredPersons.map((person, index) => {
+                    return (
+                      <>
+                        <div className="profileMenuList__item" key={index}>
+                          <button onClick={() => setActivePerson(index)} className='profileMenuList__itemImg' >
+                            <Avatar src={person.avatar} size={100} style={activePerson === index ? {border: '2px solid crimson'} : {}}/>
+                          </button>
+                        </div>
+                      </>
+                    )
+                  })}
+                </>
+              }
+            </div>
+            <div className="profileMenuList__settings">
+              <div className="profilePersons__Filter">
+                <Button size={'large'} onClick={() => {
+                  setActivePerson(0)
+                  setFilteredPersons(persons.filter(f => f.difficulty === 'easy'))
+                }}>Легкие</Button>
+                <Button size={'large'} onClick={() => {
+                  setActivePerson(0)
+                  setFilteredPersons(persons.filter(f => f.difficulty === 'normal'))
+                }}>Нормальные</Button>
+                <Button size={'large'} onClick={() => {
+                  setActivePerson(0)
+                  setFilteredPersons(persons.filter(f => f.difficulty === 'hard'))
+                }}>Сложные</Button>
+              </div>
+            </div>
           </div>
+          <div className="profilePersons__Button">
+            {screenWidth < 769 && <NavLink to='/game'>
+                <Button onClick={() => setProfile()}>
+                  Начать игру
+                </Button>
+              </NavLink>
+            }
+          </div>  
+        </div>
+        <div className="profilePreview">
           <div className="profilePreview__Name">
             {filteredPersons[activePerson].name}
             <div className="profilePreview__NameWork">
@@ -116,52 +160,52 @@ export const ProfilePage: FC = () => {
           <div className="profilePreview__Img">
             <img src={filteredPersons[activePerson].img} alt=""/>
           </div>
+          <div className="profilePreview__Stats">
+            <div className='profilePreview__StatsIncome'>
+              <div className="profilePreview__StatsTitle">
+                Доходы
+              </div>
+              <div className="profilePreview__StatsBlock">
+                <div className="profilePreview__StatsBlock__Title">Зарплата:</div>
+                <div className="profilePreview__StatsBlock__Price">${filteredPersons[activePerson].salary}</div>
+              </div>
+              <div className="profilePreview__StatsBlock">
+                <div className="profilePreview__StatsBlock__Title">Сбережения:</div>
+                <div className="profilePreview__StatsBlock__Price">${filteredPersons[activePerson].saving}</div>
+              </div>
+            </div>
+            <div className='profilePreview__StatsExpenses'>
+              <div className="profilePreview__StatsTitle">
+                Расходы
+              </div>
+              <div className="profilePreview__StatsBlock">
+                <div className="profilePreview__StatsBlock__Title">Налог:</div>
+                <div className="profilePreview__StatsBlock__Price">${tax}</div>
+              </div>
+              {filteredPersons[activePerson].expenses.map((expense, index) => {
+                return (
+                  <span key={index}>
+                    {expense.remainPrice !== 0
+                      ? <div className='profilePreview__StatsBlock'>
+                          <div className="profilePreview__StatsBlock__Title">{expense.title}:</div>
+                          <div className="profilePreview__StatsBlock__Price">${expense.remainPrice * expense.payment / 100}</div>
+                        </div>
+                      : ''}
+                  </span>
+                )
+              })}
+            </div>
+            <div className="profilePreview__StatsCash">
+              <div className="profilePreview__StatsTitle">
+                Наличные:
+              </div>
+              <div className="profilePreview__StatsBlock">
+                ${filteredPersons[activePerson].salary - taxesSummary}
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="profilePersons">
-          <div className="profilePersons__Title">
-            Доступные персонажи
-          </div>
-          <div className="profilePersons__Filter">
-            <Button size={'large'} onClick={() => {
-              setActivePerson(0)
-              setFilteredPersons(persons.filter(f => f.difficulty === 'easy'))
-            }}>Легкие</Button>
-            <Button size={'large'} onClick={() => {
-              setActivePerson(0)
-              setFilteredPersons(persons.filter(f => f.difficulty === 'normal'))
-            }}>Нормальные</Button>
-            <Button size={'large'} onClick={() => {
-              setActivePerson(0)
-              setFilteredPersons(persons.filter(f => f.difficulty === 'hard'))
-            }}>Сложные</Button>
-          </div>
-          <div className="profilePersons__Blocks">
-            {filteredPersons.length === 0
-            ? 'загрузка'
-            : <>
-                {filteredPersons.map((person, index) => {
-                  return (
-                    <>
-                      <div className="profilePersons__Block" key={index}>
-                        <button onClick={() => setActivePerson(index)} className='profilePersons__BlockImg' >
-                          <Avatar src={person.avatar} size={100} style={activePerson === index ? {border: '2px solid crimson'} : {}}/>
-                        </button>
-                      </div>
-                    </>
-                  )
-                })}
-              </>
-            }
-          </div>
-          {screenWidth > 769 && <div className="profilePersons__Button">
-            <NavLink to='/game'>
-              <Button onClick={() => setProfile()}>
-                Начать игру
-              </Button>
-            </NavLink>
-          </div>}
-        </div>
-        <div className="profileMenu">
+        <div className="profileSettings">
           <div className="profileMenu__Buttons">
             <div className="profileMenu__Button">
               <NavLink to='/'>
@@ -194,58 +238,6 @@ export const ProfilePage: FC = () => {
                 className='settingsListItem__Radio'
               />
             </div>
-          </div>
-          <div className="profileMenu__Stats">
-            <div className='profileMenu__StatsIncome'>
-              <div className="profileMenu__StatsTitle">
-                Доходы
-              </div>
-              <div className="profileMenu__StatsBlock">
-                <div className="profileMenu__StatsBlock__Title">Зарплата:</div>
-                <div className="profileMenu__StatsBlock__Price">${filteredPersons[activePerson].salary}</div>
-              </div>
-              <div className="profileMenu__StatsBlock">
-                <div className="profileMenu__StatsBlock__Title">Сбережения:</div>
-                <div className="profileMenu__StatsBlock__Price">${filteredPersons[activePerson].saving}</div>
-              </div>
-            </div>
-            <div className='profileMenu__StatsExpenses'>
-              <div className="profileMenu__StatsTitle">
-                Расходы
-              </div>
-              <div className="profileMenu__StatsBlock">
-                <div className="profileMenu__StatsBlock__Title">Налог:</div>
-                <div className="profileMenu__StatsBlock__Price">${tax}</div>
-              </div>
-              {filteredPersons[activePerson].expenses.map((expense, index) => {
-                return (
-                  <span key={index}>
-                    {expense.remainPrice !== 0
-                      ? <div className='profileMenu__StatsBlock'>
-                          <div className="profileMenu__StatsBlock__Title">{expense.title}:</div>
-                          <div className="profileMenu__StatsBlock__Price">${expense.remainPrice * expense.payment / 100}</div>
-                        </div>
-                      : ''}
-                  </span>
-                )
-              })}
-            </div>
-            <div className="profileMenu__StatsCash">
-              <div className="profileMenu__StatsTitle">
-                Наличные:
-              </div>
-              <div className="profileMenu__StatsBlock">
-                ${filteredPersons[activePerson].salary - taxesSummary}
-              </div>
-            </div>
-          </div>
-          <div className="profilePersons__Button">
-            {screenWidth < 769 && <NavLink to='/game'>
-                <Button onClick={() => setProfile()}>
-                  Начать игру
-                </Button>
-              </NavLink>
-            }
           </div>
         </div>
       </div>
