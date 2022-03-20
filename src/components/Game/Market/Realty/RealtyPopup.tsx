f3ba794a5fe31a6e3e67b4f089d010db68ae995f
андрей дьяков <brodiaga_0880@mail.ru>
2022-03-20 10:43:11 +0300

    local version

--- src/components/Game/Market/Realty/RealtyPopup.tsx
+++ src/components/Game/Market/Realty/RealtyPopup.tsx
@@ -1,8 +1,7 @@
 import { CloseOutlined } from "@ant-design/icons"
 import { FC } from "react"
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { settingsActions } from "../../../../redux/settings-reducer"
-import { getConstTimeSpeedSelector } from "../../../../redux/settings-selector"
 type RealtyPopupType = {
   isRealtyToSell: boolean // окно для покупки или продажи? 
   setIsRealtyShown: (isRealtyShown: boolean) => void // закрытие окна...
@@ -23,6 +22,10 @@ export const RealtyPopup: FC<RealtyPopupType> = ({isRealtyToSell, setIsRealtySho
           <div className="realtyPopupBlock__Close">
             <CloseOutlined onClick={onCloseClick} />
           </div>
+          {/* 
+            в зависимости от того, покупаем или продаем мы недвижиомость, мы будем выводить, список нашей недвижимости...
+            окошко в котором будет отображатся недвижимость, если мы хотим ее купить или продать...
+          */}
         </div>
       </div>
     </>
